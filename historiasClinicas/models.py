79832ea3bab8fb9bde2f661d6a3a7f54be4d8ad8
unknown <cmora25@uan.edu.co>
2020-06-08 20:42:38 -0500

    proyecto final

--- historiasClinicas/models.py
+++ historiasClinicas/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class HistoriaClinica (models.Model):
     fecha = models.DateField()
-    Evento = models.CharField(max_length=20)
+    evento = models.CharField(max_length=20)
 
 class Paciente (models.Model):
     nombre = models.CharField(max_length=20)
@@ -12,13 +12,13 @@ class Paciente (models.Model):
     direccion = models.CharField(max_length=20)
     telefono = models.CharField(max_length=20)
 
-    historiaClinica = models.ForeignKey(to=HistoriaClinica,on_delete=models.CASCADE, null=False, blank=False)
+    historiaClinica = models.ForeignKey(to=HistoriaClinica,on_delete=models.CASCADE, null=True, blank=True)
 
 class Medico (models.Model):
     nombre = models.CharField(max_length=20)
     cedula = models.CharField(max_length=20)
     tarjetaProfesional = models.IntegerField()
-    Especialidad = models.CharField(max_length=20)
+    especialidad = models.CharField(max_length=20)
 
     paciente = models.ForeignKey(to=Paciente, on_delete=models.CASCADE, null=False, blank=False)
 
--- historiasClinicas/serializers.py
+++ historiasClinicas/serializers.py
@@ -1,25 +1,25 @@
 from rest_framework import routers, serializers, viewsets
 from .models import *
 
-class HistoriaClinicaSerializers (serializers.HyperlinkedModelSerializer):
+class HistoriaClinicaSerializers (serializers.ModelSerializer):
     class Meta:
         model = HistoriaClinica
         fields = ['id', 'fecha', 'evento']
         pass
 
-class PacienteSerializers (serializers.HyperlinkedModelSerializer):
+class PacienteSerializers (serializers.ModelSerializer):
     class Meta:
         model = Paciente
-        fields = ['id', 'nombre', 'cedula', 'ciudad', 'direccion', 'telefono' ]
+        fields = ['id', 'nombre', 'cedula', 'ciudad', 'direccion', 'telefono', 'historiaClinica' ]
         pass
 
-class MedicoSerializers (serializers.HyperlinkedModelSerializer):
+class MedicoSerializers (serializers.ModelSerializer):
     class Meta:
         model = Medico
-        fields = ['id', 'nombre', 'cedula', 'tarjetaProfesional', 'Especialidad', 'paciente']
+        fields = ['id', 'nombre', 'cedula', 'tarjetaProfesional', 'especialidad', 'paciente']
         pass
 
-class CentroMedicoSerializers (serializers.HyperlinkedModelSerializer):
+class CentroMedicoSerializers (serializers.ModelSerializer):
     class Meta:
         model = CentroMedico
         fields = ['id', 'nombre', 'ciudad', 'direccion', 'telefono', 'paciente', 'medico']
